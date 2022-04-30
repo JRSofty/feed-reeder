5b40409135c036911697e3ae292766705eacfa60
Jason Reed <jrsofty@gmail.com>
2022-04-30 15:57:07 +0200

    Adds Foreign Key Constraints to the tables for parent referencing

--- feed-reeder-persistence/SQL/Flyway/MySQL/init/V1.1__FeedReederInit.sql
+++ feed-reeder-persistence/SQL/Flyway/MySQL/init/V1.1__FeedReederInit.sql
@@ -2,8 +2,9 @@ CREATE TABLE `group_feed`(
 	`id` BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`title` VARCHAR(100) NOT NULL,
 	`description` VARCHAR(255),
-	`parent_id` BIGINT(20)
-);
+	`parent_id` BIGINT(20),
+	CONSTRAINT `fk_gf_pid` FOREIGN KEY (parent_id) REFERENCES group_feed (id)
+)ENGINE = InnoDB;
 
 CREATE TABLE `web_feed`(
 	`id` BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -17,8 +18,9 @@ CREATE TABLE `web_feed`(
 	`last_update_success` DATETIME,
 	`last_update_failure` DATETIME,
 	`last_failure_reason` VARCHAR(255),
-	`cron_expression` VARCHAR(40) NOT NULL DEFAULT '0 15 10 * * ?'
-);
+	`cron_expression` VARCHAR(40) NOT NULL DEFAULT '0 15 10 * * ?',
+	CONSTRAINT `fk_wf_pid` FOREIGN KEY (parent_id) REFERENCES group_feed (id)
+)ENGINE = InnoDB;
 
 CREATE TABLE `feed_item`(
 	`id` BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -27,5 +29,6 @@ CREATE TABLE `feed_item`(
 	`link_url` VARCHAR(255) NOT NULL,
 	`pub_date` DATETIME NOT NULL,
 	`description` TEXT NOT NULL,
-	`viewed` TINYINT(2) NOT NULL
-);
+	`viewed` TINYINT(2) NOT NULL,
+	CONSTRAINT `fk_fi_pid` FOREIGN KEY (parent_id) REFERENCES web_feed (id)
+)ENGINE = InnoDB;
