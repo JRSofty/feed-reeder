35fc7da01ea4fc875c6a29167c3104fb485e9f27
Jason Reed <jrsofty@gmail.com>
2022-05-01 09:22:35 +0200

    Extends NodeNameFilter to allow looking for certain attributes

--- feed-reeder-models/src/main/java/com/jrsofty/web/feeder/models/xml/NodeNameFilter.java
+++ feed-reeder-models/src/main/java/com/jrsofty/web/feeder/models/xml/NodeNameFilter.java
@@ -1,19 +1,31 @@
 package com.jrsofty.web.feeder.models.xml;
 
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.traversal.NodeFilter;
 
 public class NodeNameFilter implements NodeFilter {
 
     private final String nodeName;
+    private String attribute = null;
 
     public NodeNameFilter(String nodeName) {
+        this(nodeName, null);
+    }
+
+    public NodeNameFilter(String nodeName, String attribute) {
         this.nodeName = nodeName;
+        this.attribute = attribute;
     }
 
     @Override
     public short acceptNode(Node n) {
-        if (n.getNodeName().equals(this.nodeName)) {
+        if (this.attribute != null) {
+            final NamedNodeMap attr = n.getAttributes();
+            if ((n.getNodeName().equals(this.nodeName) && (attr != null)) && (attr.getNamedItem(this.attribute) != null)) {
+                return NodeFilter.FILTER_ACCEPT;
+            }
+        } else if (n.getNodeName().equals(this.nodeName)) {
             return NodeFilter.FILTER_ACCEPT;
         }
         return NodeFilter.FILTER_REJECT;
