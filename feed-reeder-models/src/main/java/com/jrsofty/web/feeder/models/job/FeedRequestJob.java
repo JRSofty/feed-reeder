4130a79b1e090087e7a2035f0c8d25bca6cc8730
Jason Reed <jrsofty@gmail.com>
2022-05-02 21:59:24 +0200

    Includes Quartz to models for Job extension

---
+++ feed-reeder-models/src/main/java/com/jrsofty/web/feeder/models/job/FeedRequestJob.java
@@ -0,0 +1,14 @@
+package com.jrsofty.web.feeder.models.job;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class FeedRequestJob implements Job {
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+
+    }
+
+}
