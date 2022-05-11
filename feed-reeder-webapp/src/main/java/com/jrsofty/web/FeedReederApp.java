b5bc6f4ee306fa76f097c04ce2992347faef2647
Jason Reed <jrsofty@gmail.com>
2022-05-11 21:42:31 +0200

    Changes some spring annotations

--- feed-reeder-webapp/src/main/java/com/jrsofty/web/FeedReederApp.java
+++ feed-reeder-webapp/src/main/java/com/jrsofty/web/FeedReederApp.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = { "com.jrsofty.web" })
+
 public class FeedReederApp {
     public static void main(String[] args) {
         SpringApplication.run(FeedReederApp.class, args);
