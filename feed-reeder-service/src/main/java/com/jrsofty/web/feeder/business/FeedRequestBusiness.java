64edfee5c39dcbd85ef50b9f80e4ab27cba6014a
Jason Reed <jrsofty@gmail.com>
2022-05-11 22:05:06 +0200

    Removes the logger since it's not the right one.

--- feed-reeder-service/src/main/java/com/jrsofty/web/feeder/business/FeedRequestBusiness.java
+++ feed-reeder-service/src/main/java/com/jrsofty/web/feeder/business/FeedRequestBusiness.java
@@ -2,8 +2,6 @@ package com.jrsofty.web.feeder.business;
 
 import javax.transaction.Transactional;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,8 +12,6 @@ import com.jrsofty.web.feeder.persistence.dao.impl.WebFeedDAO;
 @Component
 @Transactional
 public class FeedRequestBusiness implements FeedRequestInterface {
-
-    private static Logger LOG = LoggerFactory.getLogger(FeedRequestBusiness.class);
 
     @Autowired
     private FeedItemDAO feedItemDAO;
@@ -31,7 +27,7 @@ public class FeedRequestBusiness implements FeedRequestInterface {
     @Transactional
     @Override
     public void processFeedData(String data) {
-        FeedRequestBusiness.LOG.info(data);
+        System.out.println(data);
     }
 
 }
