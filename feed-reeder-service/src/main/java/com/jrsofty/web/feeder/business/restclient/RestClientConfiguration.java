9888260990113154ab49e5bbe4e022409606fcb2
Jason Reed <jrsofty@gmail.com>
2022-12-31 13:52:12 +0100

    Update Security Config

--- feed-reeder-service/src/main/java/com/jrsofty/web/feeder/business/restclient/RestClientConfiguration.java
+++ feed-reeder-service/src/main/java/com/jrsofty/web/feeder/business/restclient/RestClientConfiguration.java
@@ -3,13 +3,15 @@ package com.jrsofty.web.feeder.business.restclient;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@PropertySource("file:${feeder.home}/conf/feeder.properties")
 public class RestClientConfiguration {
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    public RestTemplate restTemplate(final RestTemplateBuilder builder) {
         return builder.build();
     }
 
--- feed-reeder-service/src/main/java/com/jrsofty/web/feeder/security/SecurityConfig.java
+++ feed-reeder-service/src/main/java/com/jrsofty/web/feeder/security/SecurityConfig.java
@@ -1,31 +1,19 @@
 package com.jrsofty.web.feeder.security;
 
-import java.io.File;
-
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import com.jrsofty.web.feeder.commons.logging.LogUtil;
 
 @Configuration
+@PropertySource("file:${feeder.home}/conf/feeder.properties")
 public class SecurityConfig {
 
     private static Logger LOG = LogUtil.getLogger(SecurityConfig.class);
 
     @Value("${feeder.home}")
     private String feederHome;
-
-    @Bean
-    public void checkInitUser() {
-        final String initPropertiesPath = new StringBuilder().append(this.feederHome).append("/init/init.properties").toString();
-        final File propertiesFile = new File(initPropertiesPath);
-        if (propertiesFile.isFile() && propertiesFile.exists()) {
-            SecurityConfig.LOG.info("File exists");
-        } else {
-            SecurityConfig.LOG.info("File Not Found");
-        }
-    }
 
 }
