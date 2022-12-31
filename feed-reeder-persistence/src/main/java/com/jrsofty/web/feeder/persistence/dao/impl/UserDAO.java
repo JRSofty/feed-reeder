cf28bf3e5e7d32a87a897177b20a08175464d2d8
Jason Reed <jrsofty@gmail.com>
2022-12-31 12:53:46 +0100

    Implements basic UserDAO

--- feed-reeder-persistence/src/main/java/com/jrsofty/web/feeder/persistence/dao/impl/UserDAO.java
+++ feed-reeder-persistence/src/main/java/com/jrsofty/web/feeder/persistence/dao/impl/UserDAO.java
@@ -2,26 +2,41 @@ package com.jrsofty.web.feeder.persistence.dao.impl;
 
 import java.util.List;
 
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.jrsofty.web.feeder.models.domain.User;
 
+@Repository("User")
+@Transactional
 public class UserDAO extends AbstractGenericDAO<User> {
 
     @Override
+    @Transactional
     public void delete(final long id) {
-        // TODO Auto-generated method stub
-
+        final User user = this.em.find(User.class, id);
+        this.em.remove(user);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User findById(final long id) {
-        // TODO Auto-generated method stub
-        return null;
+        return this.em.find(User.class, id);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<User> findAll() {
-        // TODO Auto-generated method stub
-        return null;
+        final TypedQuery<User> query = this.em.createQuery("SELECT u FROM User u", User.class);
+        return query.getResultList();
+    }
+
+    @Transactional(readOnly = true)
+    public long countUsers() {
+        final TypedQuery<Long> query = this.em.createQuery("SELECT COUNT(*) FROM User", Long.class);
+        return query.getSingleResult();
     }
 
 }
